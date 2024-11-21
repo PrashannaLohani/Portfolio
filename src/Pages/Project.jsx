9d971d7274f77b79b397ba1048fb386d9d9e1cd4
PrashannaLohani <plohani2003@gmail.com>
2024-11-21 13:41:48 +0545

    Project listed

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -78,7 +78,8 @@ export default function Project() {
                   key={project.id}
                   sx={{
                     maxWidth: 345,
-                    bgcolor: "#FFFACD",
+                    background:
+                      "linear-gradient(0deg, rgba(237,223,224,1) 33%, rgba(255,255,255,1) 100%)",
                     boxShadow:
                       "rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px",
                     borderRadius: "1rem",
